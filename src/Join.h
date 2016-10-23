b08dcf33329ec9bcdff61003fa5fbdd017a8a865
matheusgda <matheusgandrade@poli.ufrj.br>
2016-10-22 22:37:14 -0200

    implement brute-force join

---
+++ src/Join.h
@@ -0,0 +1,17 @@
+#ifndef JOIN_H
+#define JOIN_H
+
+#include "Relation.h"
+#include "Index.h"
+#include <utility>
+/* A join receive two relations and returns the joined relation of both.*/
+
+std::pair<unsigned, unsigned> bruteForceJoin (DenseIndex *leftEntity, DenseIndex *rightEntity
+											  const binFileHandler leftHandler, const binFileHandler rightHandler,
+											  binFileHandler outHandler,
+											  Relation lRelation, Relation rRelation,
+											  unsigned rPosition);
+
+unsigned computeTime (const std::pair<unsigned, unsigned> values);
+
+#endif // JOIN_H
