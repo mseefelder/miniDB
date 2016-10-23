8173520ec4eda7fd68a2aad9e4056c41c449f738
matheusgda <matheusgandrade@poli.ufrj.br>
2016-10-23 11:34:52 -0200

    Implement first version of Hash Join completely in memory using an unordered_multimap.

--- src/Join.cpp
+++ src/Join.cpp
@@ -1,4 +1,5 @@
 #include "Join.h"
+#include <unordered_map> 
 
 #define BLOCK_SIZE 4096
 std::pair<unsigned, unsigned>
@@ -79,6 +80,59 @@ mergeSortJoin (DenseIndex *leftEntity, DenseIndex *rightEntity,
 
 			++j;
 		}
+	}
+
+	return std::make_pair(seek, blocks / BLOCK_SIZE);
+}
+
+
+std::pair<unsigned, unsigned>
+hashJoin (DenseIndex *leftEntity, DenseIndex *rightEntity,
+		  BinFileHandler leftHandler, BinFileHandler rightHandler,
+		  BinFileHandler outHandler, Relation lRelation,
+		  Relation rRelation, unsigned rPosition) {
+
+	unsigned seek = 0, blocks = 0;
+
+	std::unordered_multimap	<int, unsigned> table;
+
+	const unsigned bytes = lRelation.getTupleSize() + 
+		rRelation.getTupleSize() - rRelation.getAttSize(rPosition);
+
+	// partioning phase
+	for (const auto & lElement : leftEntity -> index)
+		table.insert(std::pair<int, unsigned> (lElement.first, lElement.second));
+
+	// probing phase
+	for (const auto & rElement : rightEntity -> index) {
+
+		size_t bucketIndex = std::hash<int>{} (rElement.first);
+		string buffer;
+
+		// avoid seg faults
+		if (bucketIndex < table.size()) {
+
+			for (auto localIt = table.begin(bucketIndex); 
+				localIt != table.end(bucketIndex); ++localIt) {
+
+				// just a testing snippet to make sure the hash function is the same
+				if ((*localIt).second != rElement.second) 
+					std::cout << "Erro: mismatched hash functions!!!" << std::endl;
+
+				// move binfile stream to element
+				leftHandler.input.seekg((*localIt).second, leftHandler.input.beg);
+				rightHandler.input.seekg(rElement.second, rightHandler.input.beg);
+				seek += 2;
+
+				// create joined tuple
+				buffer += lRelation.readRegistry(leftHandler);
+				buffer += rRelation.readRegistry(rightHandler, true, rPosition);
+				blocks += bytes;
+			}
+		}
+
+		// write output to disk
+		outHandler.output.write(buffer.c_str(), buffer.size());
 	}
 
 	return std::make_pair(seek, blocks / BLOCK_SIZE);
